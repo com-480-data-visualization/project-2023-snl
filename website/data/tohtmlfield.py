eb718e7b8500e0c3e24b90f4536b28b9bb734b04
kb29x37 <louis.coulon@epfl.ch>
2023-06-03 19:49:05 +0200

    Better form

--- website/data/tohtmlfield.py
+++ website/data/tohtmlfield.py
@@ -8,11 +8,11 @@ data.sort_values(["GHG emissions per kilogram (Poore & Nemecek, 2018)"],axis=0,a
 print(data)
 
 for index, row in data.iterrows():
-    print("<label>" + row[0]+ ":")
-    print("  <input id=\"" + row[0] + "\" name=\"" + row[0] +"\" type=\"number\" value=\"0\">")
-    print("  <text>g/week</text>")
-    print("</label>")
-    print("<div/>")
+    print("<p>")
+    print("<label>" + row[0]+ ":</label>")
+    print("<input id=\"" + row[0] + "\" name=\"" + row[0] +"\" type=\"number\" value=\"0\">")
+    print("<text>g/week</text>")
+    print("</p>")
 
 for index, row in data.iterrows():
     print("[\"" + row[0] + "\", \"Food\"" + "],")
