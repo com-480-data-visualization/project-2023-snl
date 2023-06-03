d7416d1f6ee9aab920205e8a62e3f98284a47de6
kb29x37 <louis.coulon@epfl.ch>
2023-06-03 18:03:22 +0200

    Improving the calculator

--- website/calculator.js
+++ website/calculator.js
@@ -47,9 +47,14 @@ let classes = new Map ([
     ["Nuts", "Food"],
     ["Apples", "Food"],
     ["Root Vegetables", "Food"],
-    ["Citrus Fruit", "Food"]
+    ["Citrus Fruit", "Food"],
+    ["Energy", "Energy"],
+    ["Country", "Energy"]
 ]);
 
+var count = 0;
+
+// g / km
 function getTransport(form, transport) {
     let tForm = Object.fromEntries(Object.entries(form).filter(([k,v]) => classes.get(k) == "Transport"));
 
@@ -65,44 +70,49 @@ function getTransport(form, transport) {
         qString = carSize + " " + carType;
     }
 
-    console.log(tForm);
-
     delete tForm["CarType"];
     delete tForm["CarFuel"];
     delete tForm["CarSize"];
     delete tForm["CarKm"];
 
-    console.log("deleted: ");
-    console.log(tForm);
-    console.log(transport);
-
     tForm = Object.entries(tForm).map(
         ([k,v]) => {
-            return parseInt(transport[k]) * parseInt(v);
+            return [k, (parseInt(transport[k]) * parseInt(v)) / 1000];
         }
     );
 
-    tForm["Car"] = km * transport[qString];
+    tForm.push(["Car", (km * transport[qString]) / 1000]);
 
     return tForm;
 }
 
+// kg / kg
 function getFood(form, food) {
     let tForm = Object.fromEntries(Object.entries(form).filter(([k,v]) => classes.get(k) == "Food"));
 
-    tForm = Object.entries(tForm).map(
+    tForm = Object.entries(tForm).map (
         ([k,v]) => {
-            return parseInt(food[k]) * parseInt(v) * 52;
+            return [k, parseInt(food[k]) * parseInt(v) * 52 / 1000];
         }
     );
 
     return tForm;
 }
 
+// kg / KWh
 function getEnergy(form, energy) {
     let tForm = Object.fromEntries(Object.entries(form).filter(([k,v]) => classes.get(k) == "Energy"));
+    let country = tForm["Country"];
 
+    console.log(country);
 
+    delete tForm["Country"];
+
+    tForm = Object.entries(tForm).map (
+        ([k,v]) => {
+            return [k, parseInt(energy[country]) * parseInt(v)];
+        }
+    );
 
     return tForm;
 }
@@ -110,21 +120,23 @@ function getEnergy(form, energy) {
 function cleanForBarChart(form, transport, food, energy) {
     let tr = getTransport(form, transport);
     let fd = getFood(form, food);
+    let en = getEnergy(form, energy);
 
-    console.log(fd);
+    let cleanForm = tr.concat(fd).concat(en);
 
-    throw new Error();
+    return cleanForm;
 }
 
-function getData(form) {
-    var formD = new FormData(form);
-    var formData = Object.fromEntries(formD);
-
-    let foodCsv = readData(function (foodCsv, energyCsv, transportCsv) {
+function drawPlot(formData) {
+    readData(function (foodCsv, energyCsv, transportCsv) {
         // set the dimensions and margins of the graph
-        var margin = {top: 10, right: 30, bottom: 20, left: 50},
-            width = 460 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+        var margin = {top: 30, right: 100, bottom: 70, left: 150},
+            width = 1000 - margin.left - margin.right,
+            height = 1000 - margin.top - margin.bottom;
+
+        var maxVal = 0;
+
+        d3.select("#footprint").html("");
 
         // append the svg object to the body of the page
         var svg = d3.select("#footprint")
@@ -135,67 +147,85 @@ function getData(form) {
             .attr("transform",
                   "translate(" + margin.left + "," + margin.top + ")");
 
-        console.log(formData);
         formData = cleanForBarChart(formData, transportCsv, foodCsv, energyCsv);
+        console.log(formData);
 
-        // List of subgroups = header of the csv files = soil condition here
-        var classes = ["Food", "Transport", "Energy"];
+        console.log(JSON.stringify(formData));
 
-        // List of groups = species here = value of the first column called group -> I show them on the X axis
-        var groups = d3.map(d3.entries(data), function(d) {
-            console.log(d.key);
-            return classes[d.key];
-        }).keys();
+        for(var i = 0; i < formData.length; i++) {
+            if(formData[i][1] > maxVal) {
+                maxVal = formData[i][1];
+            }
+        }
 
-        console.log(groups);
+        var sortedData = formData.sort(function(d0, d1) {return d0[1] - d1[1];}).reverse();
+        sortedData = sortedData.filter(([k,v]) => v != 0);
 
-        return;
-
-        // Add X axis
-        var x = d3.scaleBand()
-            .domain(groups)
-            .range([0, width])
-            .padding([0.2]);
+        var x = d3.scaleLinear()
+            .domain([0, maxVal + 10])
+            .range([ 0, width - 200]);
 
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x).tickSizeOuter(0));
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
 
-        // Add Y axis
-        var y = d3.scaleLinear()
-            .domain([0, 60])
-            .range([ height, 0 ]);
+        var y = d3.scaleBand()
+            .range([ 0, height ])
+            .domain(sortedData.map(function(data) { return data[0]; }))
+            .padding(.1);
+
         svg.append("g")
             .call(d3.axisLeft(y));
 
-        // color palette = one color per subgroup
-        var color = d3.scaleOrdinal()
-            .domain(subgroups)
-            .range(['#e41a1c','#377eb8','#4daf4a']);
+        var colors = new Map([["Transport", "#51afef"], ["Food", "#da8548"], ["Energy", "#98be65"]]);
 
-        //stack the data? --> stack per subgroup
-        var stackedData = d3.stack()
-            .keys(subgroups)(data);
+        svg.selectAll("bar")
+            .data(sortedData)
+            .enter()
+            .append("rect")
+            .style("fill", function(d) {
+                console.log(d[0] + " -> " + colors.get(classes.get(d[0])));
+                return colors.get(classes.get(d[0]));
+            })
+            .attr("x", x(0) )
+            .attr("y", function(d) { return y(d[0]); })
+            .attr("width", function(d) { return x(d[1]); })
+            .attr("height", y.bandwidth() );
 
-        // Show the bars
-        svg.append("g")
-            .selectAll("g")
-        // Enter in the stack data = loop key per key = group per group
-            .data(stackedData)
-            .enter().append("g")
-            .attr("fill", function(d) { return color(d.key); })
-            .selectAll("rect")
-        // enter a second time = loop subgroup per subgroup to add all rectangles
-            .data(function(d) { return d; })
-            .enter().append("rect")
-            .attr("x", function(d) { return x(d.data.group); })
-            .attr("y", function(d) { return y(d[1]); })
-            .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-            .attr("width",x.bandwidth());
+        svg.append("rect").attr("x",600).attr("y",50).attr("width", 20)
+            .attr("height", 10).style("fill", "#da8548");
+        svg.append("rect").attr("x",600).attr("y",80).attr("width", 20)
+            .attr("height", 10).style("fill", "#51afef");
+        svg.append("rect").attr("x",600).attr("y",110).attr("width", 20)
+            .attr("height", 10).style("fill", "#98be65");
+
+        svg.append("text").attr("x", 630).attr("y", 60).text("Food")
+            .style("font-size", "15px").attr("alignment-baseline","middle");
+        svg.append("text").attr("x", 630).attr("y", 90).text("Transport")
+            .style("font-size", "15px").attr("alignment-baseline","middle");
+        svg.append("text").attr("x", 630).attr("y", 120).text("Energy")
+            .style("font-size", "15px").attr("alignment-baseline","middle");
+
     });
+}
+
+function getData(form) {
+    var formD = new FormData(form);
+    var formData = Object.fromEntries(formD);
+
+    drawPlot(planeMeat);
+    // drawPlot(formD);
+    // drawPlot(formD);
 }
 
 document.getElementById("form").addEventListener("submit", function (e) {
     e.preventDefault();
     getData(e.target);
 });
+
+document.getElementById("submit").click();
+
+
